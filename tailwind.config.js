b5908d1d609a9481ef4cbcd265d0b9eeb711d0fc
Peter Curik <petocurik@gmail.com>
2024-04-02 08:34:38 +0200

    init tailwind + styles

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{ts,html}', './libs/ui/src/**/*.{ts,html}'],
+  theme: {
+    fontSize: {
+      h1: ['5rem', '7rem'],
+      h2: ['2.5rem', '2.75rem'],
+      h3: ['1.75rem', '2rem'],
+      h4: ['1.25rem', '1.5rem'],
+      h5: ['0.85rem', '1rem'],
+      h6: ['0.6875rem', '0.875rem'],
+    },
+    fontFamily: {
+      sans: 'Quicksand, sans-serif',
+    },
+    colors: {
+      primary: {
+        white: '#fff',
+        black: '#000000',
+        red: '#aa043b',
+      },
+      secondary: {
+        lightGrey: '#f3f3f3',
+        darkGrey: '#282828',
+      },
+    },
+  },
+  plugins: [],
+};
